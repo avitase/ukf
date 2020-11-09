3b140030d3b66c61f4766ee9d39e54474add52a4
Nis Meinert <nis.meinert@gmail.com>
2020-11-09 16:46:10 +0100

    add init_ukf method

--- example/helper/ukf.py
+++ example/helper/ukf.py
@@ -52,3 +52,71 @@ class SimpleUKFCell(ukf.UKFCell):
 class SimpleUKFRNN(ukf.KFRNN):
     def __init__(self, *args, **kwargs):
         super(SimpleUKFRNN, self).__init__(cell=SimpleUKFCell(*args, **kwargs))
+
+
+def init_ukf(*, batch_size, process_noise, measurement_noise, debug=True):
+    def _constrain_process_noise(grad):
+        """
+        Constrain process noise
+
+        Constrains:
+         (1)     (0, 0) and (1, 1): common average
+         (2)     (2, 2) and (3, 3): common average
+         (3) off-diagonal elements: zero
+
+        Index mapping of 4x4 triangular matrix:
+        [0 1 2 3 4 5 6 7 8 9] -> [[0 - - -],
+                                  [1 2 - -],
+                                  [3 4 5 -],
+                                  [6 7 8 9]]
+        """
+        avg_pos = torch.sum(grad[:, (0, 2)], dim=1) / 2.
+        avg_vel = torch.sum(grad[:, (5, 9)], dim=1) / 2.
+        new_grad = torch.zeros_like(grad)  # (3)
+        new_grad[:, 0] = avg_pos  # (1)
+        new_grad[:, 2] = avg_pos  # (1)
+        new_grad[:, 5] = avg_vel  # (2)
+        new_grad[:, 9] = avg_vel  # (2)
+
+        return new_grad
+
+    def _constrain_measurement_noise(grad):
+        """
+        Constrain measurement noise
+
+        Constrains:
+         (1)     (0, 0) and (1, 1): common average
+         (2) off-diagonal elements: zero
+
+        Index mapping of 2x2 triangular matrix:
+        [0 1 2 3] -> [[0 -],
+                      [1 2]]
+        """
+        avg = torch.sum(grad[:, (0, 2)], dim=1) / 2.
+
+        new_grad = torch.zeros_like(grad)  # (2)
+        new_grad[:, 0] = avg  # (1)
+        new_grad[:, 2] = avg  # (1)
+
+        return new_grad
+
+    def _reinit_nans(grad):
+        sel = ~torch.isfinite(grad)
+        if torch.any(sel):
+            new_grad = torch.tensor(grad)
+            new_grad[sel] = torch.rand(new_grad[sel].shape)
+            print('Warning! Found {torch.sum(sel)} NaN values in gradient')
+            return new_grad
+
+    rnn = SimpleUKFRNN(batch_size=batch_size)
+
+    rnn.cell.process_noise.data = process_noise
+    rnn.cell.measurement_noise.data = measurement_noise
+
+    rnn.cell.process_noise.register_hook(_reinit_nans)
+    rnn.cell.process_noise.register_hook(_constrain_process_noise)
+
+    rnn.cell.measurement_noise.register_hook(_reinit_nans)
+    rnn.cell.measurement_noise.register_hook(_constrain_measurement_noise)
+
+    return rnn if debug else torch.jit.script(rnn)
