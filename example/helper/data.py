5c0be44825fa03bee57de17f9677adbc5d3747ed
Nis Meinert <nis.meinert@gmail.com>
2020-11-09 16:45:17 +0100

    bug fix: axis -> dim

--- example/helper/data.py
+++ example/helper/data.py
@@ -36,7 +36,7 @@ class DataLoader:
                 v = torch.matmul(rot, v)
 
         self._gt = torch.zeros((b, 2, n * n_repeat))
-        self._gt[:, :, 1:] = torch.cumsum(vs[:, :, :-1], axis=2)
+        self._gt[:, :, 1:] = torch.cumsum(vs[:, :, :-1], dim=2)
 
         if noise:
             self._x = torch.normal(self._gt, noise.unsqueeze(2).expand_as(self._gt))
